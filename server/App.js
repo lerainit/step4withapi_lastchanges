e8c79ecbbbb9e909f0348534de4db013c4e8acf7
Valeria Vitvytska <leranmu@gmail.com>
2022-10-20 10:57:06 +0300

    feat:add request limit

--- server/App.js
+++ server/App.js
@@ -11,7 +11,7 @@ import newPostsRouter from "./newPosts/newPostsRouter.js";
 const app = express();
 const PORT = 3001;
 app.use(express.static(resolve('static')))
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(cors())
 
 app.use('/posts',postsRouter)
